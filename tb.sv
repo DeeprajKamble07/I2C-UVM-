3fe6da17fa81aa8abeb06a8fc752c163f236a7cb
Deepraj Kamble <deeprajkamble24@gmail.com>
2026-03-20 17:18:25 +0530

    Create tb.sv

---
+++ tb.sv
@@ -0,0 +1,42 @@
+// Code your testbench here
+// or browse Examples
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "interface.sv"
+`include "sequence_item.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+`include "enivornment.sv"
+`include "test.sv"
+
+module tb;
+  logic clk;
+  intf intff(clk);
+  
+  i2c_master dut1(.clk(intff.clk),.rst(intff.rst),.rw(intff.rw),.start(intff.start),.addr(intff.addr),.datain(intff.datain),.scl(intff.scl),.sda(intff.sda),.dataout(intff.dataout));
+  
+  i2c_slave #(.SLAVE_ADDR(7'h32)) dut2 (.scl(intff.scl),. sda(intff.sda));
+  
+  initial begin
+    clk=0;
+    forever #5 clk=~clk;
+  end
+  
+  initial begin
+    uvm_config_db #(virtual intf)::set(null,"*","vif",intff);
+  end
+  
+  initial begin
+    run_test("test");
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars();
+  end
+endmodule
