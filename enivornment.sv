240771d0f812cf4483acf1ef6b63d4549d5015c7
Deepraj Kamble <deeprajkamble24@gmail.com>
2026-04-09 16:23:18 +0530

    Add coverage object to enivornment class
    Added coverage object to the environment class.

--- enivornment.sv
+++ enivornment.sv
@@ -2,6 +2,7 @@ class enivornment extends uvm_env;
   `uvm_component_utils(enivornment)
   agent agen;
   scoreboard scb;
+  coverage cov;
   
   function new(string name="enivornment", uvm_component parent);
     super.new(name,parent);
@@ -11,10 +12,12 @@ class enivornment extends uvm_env;
     super.build_phase(phase);
     agen=agent::type_id::create("agen",this);
     scb=scoreboard::type_id::create("scb",this);
+    cov=coverage::type_id::create("cov",this);
   endfunction
   
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     agen.mon.monitor_port.connect(scb.scb_port);
+    agen.mon.monitor_port.connect(cov.analysis_export);
   endfunction
 endclass
